97b1eb1d54c50cc18a8a76e0de1cdc4adf79b8d6
Prakhar19999 <prakhar.srivastava19999@gmail.com>
2021-05-10 23:07:35 +0530

    Created app,models,templates and customized database

--- Inventry_Project/Inventry_Project/urls.py
+++ Inventry_Project/Inventry_Project/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from stockmgmt import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('list_item/', views.list_item, name='list_item'),
+    path('add_items/', views.add_items, name='add_items'),
 ]
