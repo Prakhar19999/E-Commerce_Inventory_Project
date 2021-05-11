2435a840560f3d80899a3dfe259270d8345fd7b1
Prakhar19999 <prakhar.srivastava19999@gmail.com>
2021-05-11 20:15:05 +0530

    Added Crispy Forms

--- Inventry_Project/Inventry_Project/settings.py
+++ Inventry_Project/Inventry_Project/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 # Application definition
 
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'stockmgmt',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
